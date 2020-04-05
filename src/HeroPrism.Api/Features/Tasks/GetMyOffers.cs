d75bb3d600a16aed9f2469164fb6325953cf78e5
Craig Fecteau <craig.fecteau@nerdino.com>
2020-04-04 17:28:07 -0700

    WIP

--- src/HeroPrism.Api/Features/Tasks/GetMyOffers.cs
+++ src/HeroPrism.Api/Features/Tasks/GetMyOffers.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,19 +19,43 @@ namespace HeroPrism.Api.Features.Tasks
     
     public class GetMyOffersResponse
     {
-        public IEnumerable<MyRequestTaskResponse> Tasks { get; set; }
+        public IEnumerable<MyOfferTaskResponse> Tasks { get; set; }
+    }
+
+    public class MyOfferTaskResponse
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ZipCode { get; set; }
+        public CoordinateDto Coordinate { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public TaskStatuses Status { get; set; }
+        public TaskCategory Category { get; set; }
+        public string RequesterId { get; set; }
+        public ChatUserResponse Requester { get; set; }
+    }
+
+    public class ChatUserResponse
+    {
+        public string ChatId { get; set; }
+        public string UserId { get; set; }
+        public int Score { get; set; }
+        public int PictureId { get; set; }
     }
 
     public class GetMyOffersRequestHandler : IRequestHandler<GetMyOffersRequest, GetMyOffersResponse>
     {
         private readonly ICosmosStore<HelpOffered> _offeredStore;
         private readonly ICosmosStore<HelpTask> _taskStore;
+        private readonly ICosmosStore<User> _userStore;
         private readonly HeroPrismSession _session;
 
-        public GetMyOffersRequestHandler(ICosmosStore<HelpOffered> offeredStore, ICosmosStore<HelpTask> taskStore, HeroPrismSession session)
+        public GetMyOffersRequestHandler(ICosmosStore<HelpOffered> offeredStore, ICosmosStore<HelpTask> taskStore, ICosmosStore<User> userStore, HeroPrismSession session)
         {
             _offeredStore = offeredStore;
             _taskStore = taskStore;
+            _userStore = userStore;
             _session = session;
         }
         
@@ -46,34 +71,72 @@ namespace HeroPrism.Api.Features.Tasks
                 return response;
             }
 
-            var taskIds = offers.Select(o => o.TaskId).Distinct().ToList();
+            var taskIds = offers.Select(o => o.TaskId).Distinct().ToHashSet();
 
-            var tasks = await _taskStore.Query()
-                .Where(t => taskIds.Contains(t.Id))
-                .ToListAsync(cancellationToken);
+            var tasks = await _taskStore.Query().ToListAsync();
+                //.Where(t => taskIds.Contains(t.Id))
+                //.ToListAsync(cancellationToken);
 
             if (!tasks.Any())
             {
                 return response;
             }
 
-            response.Tasks = tasks?.Select(c => new MyRequestTaskResponse()
-            {
-                Id = c.Id,
-                Coordinate = new CoordinateDto()
-                {
-                    Latitude = c.ZipLocation.Position.Latitude,
-                    Longitude = c.ZipLocation.Position.Longitude
-                },
-                Description = c.Description,
-                Title = c.Title,
-                ZipCode = c.ZipCode,
-                CreateDateTime = c.CreatedDateTime,
-                Status = c.Status,
-                Category = c.Category,
-            });
+            var userIds = tasks.Select(c => c.UserId);
+            
+            var users = await _userStore.Query().Where(c => userIds.Contains(c.Id)).ToListAsync(cancellationToken);
 
+            var userLookup = users.ToDictionary(c => c.Id);
+
+            response.Tasks = MapToMyOfferTaskResponses(tasks, offers, userLookup);;
+            
             return response;
+        }
+
+        private static IEnumerable<MyOfferTaskResponse> MapToMyOfferTaskResponses(List<HelpTask> tasks, List<HelpOffered> offers, Dictionary<string, User> userLookup)
+        {
+            foreach (var task in tasks)
+            {
+                var offer = offers.FirstOrDefault(c => c.TaskId == task.Id);
+
+                if (offer == null)
+                {
+                    continue;
+                }
+
+                var taskResponse = new MyOfferTaskResponse()
+                {
+                    Id = task.Id,
+                    Coordinate = new CoordinateDto()
+                    {
+                        Latitude = task.ZipLocation.Position.Latitude,
+                        Longitude = task.ZipLocation.Position.Longitude
+                    },
+                    Description = task.Description,
+                    Title = task.Title,
+                    ZipCode = task.ZipCode,
+                    CreateDateTime = task.CreatedDateTime,
+                    Status = task.Status,
+                    Category = task.Category,
+                };
+
+                var user = userLookup[task.UserId];
+
+                if (user == null)
+                {
+                    continue;
+                }
+
+                taskResponse.Requester = new ChatUserResponse()
+                {
+                    UserId = user.Id,
+                    ChatId = offer.Id,
+                    Score = user.Score,
+                    PictureId = user.PictureId
+                };
+
+                yield return taskResponse;
+            }
         }
     }
 
